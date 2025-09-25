7e1958967f94efeccc657942568a4113cba32bd0
worldwondersph <worldwondersph@gmail.com>
2025-09-26 03:03:59 +0800

    feat(visa): basic visa page with Supabase client

---
+++ src/app/visa/page.tsx
@@ -0,0 +1,8 @@
+import VisaClient from './visa-client';
+
+export const metadata = { title: 'Visa Requirements | WorldWonders' };
+
+export default function VisaPage() {
+  return <VisaClient />;
+}
+<a className="underline" href="/visa">Visa Requirements</a>
